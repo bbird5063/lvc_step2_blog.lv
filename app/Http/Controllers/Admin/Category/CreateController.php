384e44ec410e2d175395a67a61c55c669b17bf0a
bbird5063 <bbird5063@gmail.com>
2024-10-17 02:05:29 +0300

    6. Страница создания категории(форма создания)

---
+++ app/Http/Controllers/Admin/Category/CreateController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Category; // переименовал из ...\Main 
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CreateController extends Controller
+{
+  public function __invoke()
+	{
+		//dd('111111111111111111');
+		return view('admin.categories.create');
+	}
+}
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -8,7 +8,7 @@
 		<div class="container-fluid">
 			<div class="row mb-2">
 				<div class="col-sm-6">
-					<h1 class="m-0">Dashboard</h1>
+					<h1 class="m-0">Добавление категории</h1>
 				</div><!-- /.col -->
 				<div class="col-sm-6">
 					<ol class="breadcrumb float-sm-right">
@@ -26,19 +26,20 @@
 		<div class="container-fluid">
 			<!-- Small boxes (Stat box) -->
 			<div class="row">
-
-				<div class="col-2">
-					<a href="{{ route('admin.category.create') }}" type="button" class="btn btn-block btn-primary">Добавить</a>
-				</div>
-
 				<div class="col-12">
-					Категории
+					<form action="#" class="w-25">
+						<div class="form-group">
+							<input type="text" class="form-control" name="title" id="title" placeholder="Название категории">
+							<input type="submit" class="btn btn-primary mt-3" value="Добавить">
+						</div>
+					</form>
 				</div>
 			</div>
-			<!-- /.row -->
-		</div><!-- /.container-fluid -->
-	</section>
-	<!-- /.content -->
+		</div>
+		<!-- /.row -->
+</div><!-- /.container-fluid -->
+</section>
+<!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
 
