73cfa8e1a4fa48350f9c1ee245ec6e16b9f55d19
bbird5063 <bbird5063@gmail.com>
2024-10-18 02:09:51 +0300

    8. Получение списка категории

--- app/Http/Controllers/Admin/Category/IndexController.php
+++ app/Http/Controllers/Admin/Category/IndexController.php
@@ -1,15 +1,16 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Category; // переименовал из ...\Main 
+namespace App\Http\Controllers\Admin\Category; 
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category; // добавил
 
 class IndexController extends Controller
 {
   public function __invoke()
 	{
-		//dd('111111111111111111');
-		return view('admin.categories.index');
+		$categories = Category::all(); // добавил
+		return view('admin.categories.index', compact('categories'));  // добавил compact('categories') - АРГУМЕНТ СТРОКОВЫЙ!
 	}
 }
