8f0a8fc56477d6b11ce118a33a9bd2315d7ac058
bbird5063 <bbird5063@gmail.com>
2024-10-24 04:31:41 +0300

    20. Удаление постов, используем soft delete, казус удаления колонки sqlite

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
 	use HasFactory;
+	use SoftDeletes;  // ДОБАВИЛИ
 	protected $guarded = false;  // НУЖНО! либо false, либо [] (Защита массового присвоения) Чтобы мы могли изменять данные в табл.
 	protected $table = 'posts'; // LV и так привязывает, но так принято
 
